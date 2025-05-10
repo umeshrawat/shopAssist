95f9d4994403eeb5f0574106497dd790835eda34
Milind75 <miklinddurugkar@gmail.com>
2025-05-09 22:14:47 -0500

    added improved one

--- src/shopAssistApi.py
+++ src/shopAssistApi.py
@@ -2,8 +2,14 @@ import numpy as np
 from fastapi import FastAPI, Query
 from ingestion.ingest import retrive, getModel
 from ingestion.model import SearchRequest,SearchResponse
+from fastapi.staticfiles import StaticFiles
+import os
 app = FastAPI()
-app.mount("/images", static_dir="images")
+# Calculate the absolute path to the static directory
+static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "static"))
+
+# Mount it
+app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 # Search endpoint
 @app.post("/search")
